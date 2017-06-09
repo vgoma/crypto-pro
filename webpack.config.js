8532f1e6d311d1b056d8a5ecaec01d870ca85f60
NicoBurno <nicoburno@gmail.com>
2017-06-09 12:09:41 +0000

    Добавлена минификация сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,44 @@
-module.exports = {
-    context: __dirname + '/src',
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const srcPath = path.join(__dirname, './src');
+const distPath = path.join(__dirname, './dist');
+
+const rules = [
+	{
+		test: /\.js$/,
+		loader: 'babel-loader',
+		exclude: /node_modules/
+	}
+];
+
+const plugins = [];
+
+if (isProduction) {
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true
+		})
+	);
+}
+
+let config = module.exports = {
+    context: srcPath,
     entry: './index.js',
     output: {
-        path: 'dist',
+        path: distPath,
         filename: 'crypto-pro.js',
-        library: 'CryptoPro'
+        library: 'crypto-pro',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
-    devtool: 'source-map',
-    watch: true
+    devtool: isProduction ? 'source-map' : 'eval',
+    watch: !isProduction,
+    module: {
+    	rules
+    },
+    plugins
 };
