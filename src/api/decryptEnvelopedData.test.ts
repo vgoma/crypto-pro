4ab32ada935cd3da6d59fd531bcb1ee962ebd5da
AlexeyEsin <cor3one42@gmail.com>
2022-11-07 22:40:00 +0300

    specifying encoding in createHash and enveloped data decryption

---
+++ src/api/decryptEnvelopedData.test.ts
@@ -0,0 +1,26 @@
+import 'cadesplugin';
+import { decryptEnvelopedData } from './decryptEnvelopedData';
+import { _getCadesCert } from '../helpers/_getCadesCert';
+
+const executionSteps = [Symbol('step 0'), Symbol('step 1')];
+
+const executionFlow = {
+  [executionSteps[0]]: {
+    propset_ContentEncoding: jest.fn(),
+    Decrypt: jest.fn(),
+    Content: 'decrypted',
+  },
+};
+
+window.cadesplugin.__defineExecutionFlow(executionFlow);
+window.cadesplugin.CreateObjectAsync.mockImplementation(() => {
+  return executionSteps[0];
+});
+
+describe('decryptEnvelopedData', () => {
+  test('returns unencrypted enveloped data', async () => {
+    const signature = await decryptEnvelopedData('EncryptedMessage');
+
+    expect(signature).toEqual('decrypted');
+  });
+});
